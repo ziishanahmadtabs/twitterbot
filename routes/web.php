8b0ded77abfb60dd87c2fd0d5f4e214ca01713fe
ziishanahmad <ziishanahmad@gmail.com>
2022-04-10 15:10:25 +0500

    Commit

--- routes/web.php
+++ routes/web.php
@@ -2,34 +2,6 @@
 
 Auth::routes();
 
-
-Route::post('/change_password', 'Auth\ChangePasswordController@update');
-
-    Route::get('/home', 'HomeController@index')->name('home');
-
-    Route::get('/setting', 'SettingController@index');
-    Route::post('/setting', 'SettingController@update');
-
-    Route::resource('chat', 'ChatController');
-    Route::resource('archive', 'ArchiveController');
-    Route::resource('schedule', 'ScheduleController');
-
-    Route::get('/chat/status/{id}', 'ChatController@status');
-    Route::get('/archive/status/{id}', 'ArchiveController@status');
-    Route::get('/schedule/status/{id}', 'ScheduleController@status');
-
-    Route::get('/chat_tweets', "ChatTweetController@index");
-    Route::get('/tweets', "ArchiveTweetsController@index");
-
-    Route::get('/dm', "DMController@index");
-    Route::get('/dm_config', "DMController@dmConfig");
-    Route::post('/dm_config', "DMController@updateConfig");
-
-    Route::get('/analytics', "AnalyticsController@index");
-
-    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
-
-
 Route::get('/', 'HomeController@login');
 
 Route::group(['middleware' => 'auth'], function() {
